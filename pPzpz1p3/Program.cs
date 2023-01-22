bd6bf2a4caf157c1437ed67fb1ea1018727df3f1
Pat <113007904+PattZac@users.noreply.github.com>
2023-01-22 16:21:17 +0100

    Add files via upload

---
+++ pPzpz1p3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pPzpz1p3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n, max = 1;
+            Console.WriteLine("Unesite prvi broj: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Unesite drugi broj: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            n = n1 < n2 ? n1 : n2;
+            for (int i = n; i > 1; i++) 
+            { 
+                if (n1 % i == 0 && n2 % i == 0)
+                {
+                    max = i;
+                    break;
+                }
+            }
+            Console.WriteLine("Brojevi su dljevi "+max+".");
+            Console.ReadKey();
+        }
+    }
+}
